67d8fe23f310612a1eec0cd3d7a0a24d84667443
rezimaulana <rsazrm@gmail.com>
2022-08-08 00:19:12 +0700

    route to create one

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -19,8 +19,20 @@ router.get('/:id', (req, res) => {
 })
 
 //create one
-router.post('/', (req, res) => {
-    
+router.post('/', async (req, res) => {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscribedToChannel: req.body.subscribedToChannel
+    })
+    try {
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    } catch (err) {
+        res.status(400).json({message: err.message})
+    }
+    //status 201 = successfully created an object
+    //status 200 = everything was successfull
+    //status 400 =  bad data from client something wrong with user input not something wrong on server
 })
 
 //update one
