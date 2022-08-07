7e3fad5bdfb9dd1eac3706416abab3d1b4b339c3
rezimaulana <rsazrm@gmail.com>
2022-08-07 21:15:31 +0700

    database completely done

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://apps:App1212@development.b4f5g.mongodb.net', { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
