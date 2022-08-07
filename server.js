99451614f731dd67a6e4bf6f594035fb764d7ef7
rezimaulana <rsazrm@gmail.com>
2022-08-07 18:42:34 +0700

    try connect from mongoose to mongodb atlas

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://apps:App1212@development.b4f5g.mongodb.net', { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
 app.listen(3000, () => console.log('Server Started'))
-
