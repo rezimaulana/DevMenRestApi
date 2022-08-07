c65aa3784fd05c7537577b502f01cb09bea085f0
rezimaulana <rsazrm@gmail.com>
2022-08-07 22:15:58 +0700

    configure routes to be restful api

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,31 @@
 const express = require('express')
 const router = express.Router()
 
+//get one
+router.get('/', (req, res) => {
+    res.send('Hello, World!')
+})
+
+//get all
+router.get('/:id', (req, res) => {
+    
+})
+
+//create one
+router.post('/', (req, res) => {
+    
+})
+
+//update one
+//patch update half
+//put update all
+router.patch('/:id', (req, res) => {
+    
+})
+
+//delete one
+router.delete('/:id', (req, res) => {
+    
+})
+
 module.exports = router
