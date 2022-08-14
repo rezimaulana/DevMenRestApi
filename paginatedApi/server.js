d56a2d21972370ebcf20a813b14ec85d35cff065
rezimaulana <rsazrm@gmail.com>
2022-08-15 01:15:27 +0700

    create dummy get users pagination

--- paginatedApi/server.js
+++ paginatedApi/server.js
@@ -22,5 +22,10 @@ const users = [
 app.listen(3000)
 
 app.get('/users', (req, res) =>{
-    res.json(users)
+    const page = req.query.page
+    const limit = req.query.limit
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const resultUsers = users.slice(startIndex, endIndex)
+    res.json(resultUsers)
 })
