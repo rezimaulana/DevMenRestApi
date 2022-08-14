0a6256377ed7592c75511599c13dafceab7d0f23
rezimaulana <rsazrm@gmail.com>
2022-08-15 01:32:01 +0700

    step1 next and previous page

--- paginatedApi/server.js
+++ paginatedApi/server.js
@@ -22,10 +22,28 @@ const users = [
 app.listen(3000)
 
 app.get('/users', (req, res) =>{
-    const page = req.query.page
-    const limit = req.query.limit
+    const page = parseInt(req.query.page)
+    const limit = parseInt(req.query.limit)
+
     const startIndex = (page - 1) * limit
     const endIndex = page * limit
-    const resultUsers = users.slice(startIndex, endIndex)
-    res.json(resultUsers)
+
+    const results = {}
+
+    if (endIndex < users.length) {
+        results.next = {
+            page: page + 1,
+            limit: limit
+        }
+    }
+
+    if (startIndex > 0) {
+        results.previous = {
+            page: page - 1,
+            limit: limit
+        }
+    }
+
+    results.results = users.slice(startIndex, endIndex)
+    res.json(results)
 })
