94e575d4201e3a18e0b086bd4267903a87ea7618
rezimaulana <rsazrm@gmail.com>
2022-08-14 18:44:13 +0700

    finished login function

--- passportSystem/server.js
+++ passportSystem/server.js
@@ -1,11 +1,32 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+
+const initializePassport = require('./passport-config') 
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email)
+)
 
 const users = []
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {name: 'Test'})
@@ -15,9 +36,11 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
-app.post('/login', (req, res) => {
-    
-})
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
 app.get('/register', (req, res) => {
     res.render('register.ejs')
