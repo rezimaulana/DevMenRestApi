5c9146feb77d4925c4981601a1d25fd096e1501f
rezimaulana <rsazrm@gmail.com>
2022-08-14 12:24:15 +0700

    hello world html

--- simpleWebServer/app.js
+++ simpleWebServer/app.js
@@ -1,9 +1,18 @@
 const http = require('http')
+const fs = require('fs')
 const port = 3000
 
 const server = http.createServer(function(req, res) {
-    res.write('Hello, World!')
-    res.end()
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: File not found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
 })
 
 server.listen(port, function(error) {
