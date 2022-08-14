950085bd158a6e125bebfa852ca8fa9c607589ea
rezimaulana <rsazrm@gmail.com>
2022-08-14 16:33:01 +0700

    finish register function

--- passportSystem/server.js
+++ passportSystem/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+
+const users = []
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
@@ -20,8 +23,20 @@ app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register', (req, res) => {
-    
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('register')
+    }
+    console.log(users)
 })
 
 app.listen(3000)
