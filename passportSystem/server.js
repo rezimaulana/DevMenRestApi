fff018e8d16723c0340fd7781aabc8e0d19df850
rezimaulana <rsazrm@gmail.com>
2022-08-14 19:51:54 +0700

    finished logout function

--- passportSystem/server.js
+++ passportSystem/server.js
@@ -8,6 +8,7 @@ const bcrypt = require('bcrypt')
 const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
+const methodOverride = require('method-override')
 
 const initializePassport = require('./passport-config') 
 initializePassport(
@@ -28,6 +29,7 @@ app.use(session({
 }))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(methodOverride('_method'))
 
 app.get('/', checkAuthenticated, (req, res) => {
     res.render('index.ejs', {name: req.user.name})
@@ -63,6 +65,20 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
     console.log(users)
 })
 
+/* app.delete('/logout', (req, res) =>{
+    req.logOut()
+    res.redirect('/login')
+}) */
+
+app.delete('/logout', function (req, res, next) {
+    req.logOut(function (err) {
+        if (err) {
+            return next(err);
+        }
+    res.redirect('/login');
+    });
+});
+
 function checkAuthenticated(req, res, next){
     if(req.isAuthenticated()){
         return next()
@@ -72,7 +88,7 @@ function checkAuthenticated(req, res, next){
 
 function checkNotAuthenticated(req, res, next){
     if(req.isAuthenticated()){
-        res.redirect('/')
+        return res.redirect('/')
     }
     next()
 }
