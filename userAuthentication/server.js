d4e092a1265be8014f11083eb7ce8e71ba68b2e7
rezimaulana <rsazrm@gmail.com>
2022-08-14 14:04:32 +0700

    create get users route

--- userAuthentication/server.js
+++ userAuthentication/server.js
@@ -1,4 +1,12 @@
 const express = require('express')
 const app = express()
 
+const users = [
+    {name: 'Rezi'}
+]
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
 app.listen(3000)
