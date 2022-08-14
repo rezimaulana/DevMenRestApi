326246456d9eaa3ff5b082eca4652c11bf9bc17e
rezimaulana <rsazrm@gmail.com>
2022-08-14 12:12:33 +0700

    hello world

--- simpleWebServer/app.js
+++ simpleWebServer/app.js
@@ -2,7 +2,8 @@ const http = require('http')
 const port = 3000
 
 const server = http.createServer(function(req, res) {
-
+    res.write('Hello, World!')
+    res.end()
 })
 
 server.listen(port, function(error) {
