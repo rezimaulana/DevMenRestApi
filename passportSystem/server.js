0ba26d65beb2b6c8881c13590f55ac568a86e93d
rezimaulana <rsazrm@gmail.com>
2022-08-14 16:01:56 +0700

    create login and register form with setup express work

--- passportSystem/server.js
+++ passportSystem/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {name: 'Test'})
@@ -11,8 +12,16 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
+app.post('/login', (req, res) => {
+    
+})
+
 app.get('/register', (req, res) => {
     res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+    
 })
 
 app.listen(3000)
