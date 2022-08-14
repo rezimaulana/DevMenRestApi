1e5093692204d6712709a8e3949e41d5ad1b8b2f
rezimaulana <rsazrm@gmail.com>
2022-08-14 15:43:32 +0700

    testing login and register

--- passportSystem/server.js
+++ passportSystem/server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
 
+app.set('view-engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', {name: 'Test'})
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
 })
 
 app.listen(3000)
