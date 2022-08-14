d5ab2affa22a5fa2e7aa9467755baba25a8a6c5a
rezimaulana <rsazrm@gmail.com>
2022-08-15 02:02:29 +0700

    middleware configured

--- paginatedApi/server.js
+++ paginatedApi/server.js
@@ -19,31 +19,61 @@ const users = [
     {id: 15, name: 'User 15'}
 ]
 
-app.listen(3000)
+const posts = [
+    {id: 1, name: 'Posts 1'},
+    {id: 2, name: 'Posts 2'},
+    {id: 3, name: 'Posts 3'},
+    {id: 4, name: 'Posts 4'},
+    {id: 5, name: 'Posts 5'},
+    {id: 6, name: 'Posts 6'},
+    {id: 7, name: 'Posts 7'},
+    {id: 8, name: 'Posts 8'},
+    {id: 9, name: 'Posts 9'},
+    {id: 10, name: 'Posts 10'},
+    {id: 11, name: 'Posts 11'},
+    {id: 12, name: 'Posts 12'},
+    {id: 13, name: 'Posts 13'},
+    {id: 14, name: 'Posts 14'},
+    {id: 15, name: 'Posts 15'}
+]
 
-app.get('/users', (req, res) =>{
-    const page = parseInt(req.query.page)
-    const limit = parseInt(req.query.limit)
-
-    const startIndex = (page - 1) * limit
-    const endIndex = page * limit
-
-    const results = {}
-
-    if (endIndex < users.length) {
-        results.next = {
-            page: page + 1,
-            limit: limit
-        }
-    }
-
-    if (startIndex > 0) {
-        results.previous = {
-            page: page - 1,
-            limit: limit
-        }
-    }
-
-    results.results = users.slice(startIndex, endIndex)
-    res.json(results)
+app.get('/posts', paginatedResults(posts), (req, res) =>{
+    res.json(res.paginatedResults)
 })
+
+app.get('/users', paginatedResults(users), (req, res) =>{
+    res.json(res.paginatedResults)
+})
+
+function paginatedResults(model) {
+    return(req, res, next) => {
+        const page = parseInt(req.query.page)
+        const limit = parseInt(req.query.limit)
+
+        const startIndex = (page - 1) * limit
+        const endIndex = page * limit
+
+        const results = {}
+
+        if (endIndex < model.length) {
+            results.next = {
+                page: page + 1,
+                limit: limit
+            }
+        }
+
+        if (startIndex > 0) {
+            results.previous = {
+                page: page - 1,
+                limit: limit
+            }
+        }
+
+        results.results = model.slice(startIndex, endIndex)
+
+        res.paginatedResults = results
+        next()
+    }
+}
+
+app.listen(3000)
