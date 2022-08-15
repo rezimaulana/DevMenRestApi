c29f18d80a0ae34d87b9b2dba96136fe930c2b5d
rezimaulana <rsazrm@gmail.com>
2022-08-15 15:04:55 +0700

    advanced routing 2

--- expressFramework/routes/users.js
+++ expressFramework/routes/users.js
@@ -14,9 +14,25 @@ router.post('/', (req, res) => {
     res.send('Create User')
 })
 
-router.get('/:id', (req, res) => {
+router.route('/:id')
+.get((req, res) => {
+    console.log(req.user)
     req.params.id
     res.send(`Get User With ID ${req.params.id}`)
+})
+.put((req, res) => {
+    req.params.id
+    res.send(`Update User With ID ${req.params.id}`)
+})
+.delete((req, res) => {
+    req.params.id
+    res.send(`Delete User With ID ${req.params.id}`)
+})
+
+const users = [{name: 'rezi'}, {name: 'fidya'}]
+router.param('id', (req, res, next, id) => {
+    req.user = users[id]
+    next()
 })
 
 module.exports = router
