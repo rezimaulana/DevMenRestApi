7d624f1d8051b4ebfd3ea69d251065d0218ce566
rezimaulana <rsazrm@gmail.com>
2022-08-15 09:29:49 +0700

    sending data

--- expressFramework/server.js
+++ expressFramework/server.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Test')
+    /* res.send('Test')
+    res.sendStatus(500).send('Test')
+    res.status(200).json({message: 'Error'})
+    res.json({message: 'Error'})
+    res.download('server.js')
+    */
+    
 })
 
 app.listen(3000)
