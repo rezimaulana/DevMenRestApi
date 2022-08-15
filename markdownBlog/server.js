aa16ba63d9aae3fbe7ee8822569e26984d26f24a
rezimaulana <rsazrm@gmail.com>
2022-08-15 18:03:20 +0700

    testing ejs

--- markdownBlog/server.js
+++ markdownBlog/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.send('Testing')
+    res.render('index')
 })
 
 app.listen(5000)
