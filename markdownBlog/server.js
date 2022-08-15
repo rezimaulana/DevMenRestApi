53415ab4431bb536ba1fb303f868c787cceff16a
rezimaulana <rsazrm@gmail.com>
2022-08-16 01:27:10 +0700

    create view new articles

--- markdownBlog/server.js
+++ markdownBlog/server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
         createdAt: new Date(),
         description: 'Test Decription 2'
     }]
-    res.render('index', {articles: articles})
+    res.render('articles/index', {articles: articles})
 })
 
 app.listen(5000)
