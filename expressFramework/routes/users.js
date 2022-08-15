827c793a65852bc0288686fb42cb2f267f952796
rezimaulana <rsazrm@gmail.com>
2022-08-15 14:30:58 +0700

    advanced routing

--- expressFramework/routes/users.js
+++ expressFramework/routes/users.js
@@ -10,4 +10,13 @@ router.get('/new', (req, res) => {
     res.send('User New Form')
 })
 
+router.post('/', (req, res) => {
+    res.send('Create User')
+})
+
+router.get('/:id', (req, res) => {
+    req.params.id
+    res.send(`Get User With ID ${req.params.id}`)
+})
+
 module.exports = router
