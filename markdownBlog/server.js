48f439f28d7e12c6d06ad178fd5aac44ead1a16a
rezimaulana <rsazrm@gmail.com>
2022-08-16 19:39:10 +0700

    create view index content

--- markdownBlog/server.js
+++ markdownBlog/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express()
 
@@ -14,17 +15,8 @@ app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false}))
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test Decription'
-    },
-    {
-        title: 'Test Article 2',
-        createdAt: new Date(),
-        description: 'Test Decription 2'
-    }]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({createdAt: 'desc'})
     res.render('articles/index', {articles: articles})
 })
 
