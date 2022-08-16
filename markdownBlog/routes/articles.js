75ce2486bfeb7bd4742ab5ffbec4d5a3022280bf
rezimaulana <rsazrm@gmail.com>
2022-08-16 19:21:42 +0700

    create get route by id

--- markdownBlog/routes/articles.js
+++ markdownBlog/routes/articles.js
@@ -6,8 +6,10 @@ router.get('/new', (req,res) => {
     res.render('articles/new', {article: new Article()})
 })
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    if (article == null) res.redirect('/')
+    res.render('articles/show', {article: article})
 })
 
 router.post('/', async (req, res) => {
