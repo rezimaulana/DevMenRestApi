ff7ee30f3c33a7a70668e6b30fbc1306a8e7f20c
rezimaulana <rsazrm@gmail.com>
2022-08-16 16:19:05 +0700

    create schema

---
+++ markdownBlog/models/articles.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    }
+    markdown: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+        // default: () => Date.now()
+    }
+})
