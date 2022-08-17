bfa67d26d952a978377bc727d7bd48cf47f97561
rezimaulana <rsazrm@gmail.com>
2022-08-18 00:31:52 +0700

    slugify setup

--- markdownBlog/models/article.js
+++ markdownBlog/models/article.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const marked = require('marked')
+const slugify = require('slugify')
 
 const articleSchema = new mongoose.Schema({
     title: {
@@ -16,7 +18,20 @@ const articleSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
         // default: () => Date.now()
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
     }
+})
+
+articleSchema.pre('validate', function(next) {
+    if (this.title) {
+        this.slug = slugify(this.title, {lower: true, strict: true})
+    }
+
+    next()
 })
 
 module.exports = mongoose.model('blogArticles', articleSchema)
