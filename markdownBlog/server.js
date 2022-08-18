8d62aa65261d1bf9fdc9e386705457c07e7f8950
rezimaulana <rsazrm@gmail.com>
2022-08-18 12:13:56 +0700

    create delete articles route

--- markdownBlog/server.js
+++ markdownBlog/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -14,6 +15,8 @@ db.once('open', () => console.log('Connected to Database'))
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false}))
+
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
     const articles = await Article.find().sort({createdAt: 'desc'})
